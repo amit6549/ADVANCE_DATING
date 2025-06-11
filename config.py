b33237bb6e81b2e33ffbb7e5180b85f8bb09fe62
amit6549 <68516036+amit6549@users.noreply.github.com>
2025-06-11 18:03:11 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,8 +2,8 @@ import os
 from dotenv import load_dotenv
 load_dotenv()
 
-API_TOKEN = os.getenv("API_TOKEN")
-ADMIN_IDS = list(map(int, os.getenv("ADMIN_IDS", "").split(',')))
-CHANNEL_ID = os.getenv("CHANNEL_ID")
+API_TOKEN = os.getenv("7699795429:AAG1DDqtYAjUkI-x79ZiPpsobgCs9eh94iQ")
+ADMIN_IDS = list(map(int, os.getenv("868578453", "").split(',')))
+CHANNEL_ID = os.getenv("-1001646141355")
 BINANCE_API_KEY = os.getenv("BINANCE_API_KEY")
 BINANCE_SECRET = os.getenv("BINANCE_SECRET")
